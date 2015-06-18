6fec873bf7a6dc128a85c6e2a2a4ee3cdb45aee2
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-18 15:24:43 -0600

    this might work?

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('*', function(channel, msg){
+    io.emit(channel, msg);
   });
 });
 
